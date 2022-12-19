32576fbc4c74318a42566ed7e173d362841002a5
Pavkum007 <95925980+Pavkum007@users.noreply.github.com>
2022-12-19 15:00:32 +0530

    Update upgrade.ps1

--- upgrade.ps1
+++ upgrade.ps1
@@ -1,3 +1,7 @@
 param ($windowsversion)
 write-host $windowsversion
 mkdir C:\$windowsversion
+try{
+    Get-WindowsImage -ImagePath 'C:\da'
+}
+catch{Write-Output $_ | Out-File -FilePath D:\test.txt -Append}
